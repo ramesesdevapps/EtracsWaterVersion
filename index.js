007fa088b13128f461d33dec5684f3610a6941f6
Kevin Abaquita <nivekbusinesses@gmail.com>
2025-03-19 15:21:12 +0800

    version to 1.0.4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ latestVersion: "1.0.3" });
+    res.json({ latestVersion: "1.0.4" });
 });
 
 const PORT = process.env.PORT || 3000;
