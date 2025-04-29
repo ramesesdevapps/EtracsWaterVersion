00a6c65232473b2c3a9046bd08da680fb3fcfcad
Kevin Abaquita <nivekbusinesses@gmail.com>
2025-04-29 09:28:04 +0800

    version from 1.0.6 to 1.0.7

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ latestVersion: "1.0.6", date: "04-10-25" });
+    res.json({ latestVersion: "1.0.7", date: "04-28-25" });
 });
 
 const PORT = process.env.PORT || 3000;
