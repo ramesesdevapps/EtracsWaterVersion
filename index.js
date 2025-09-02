cb95d8fc946bb67fb2543603b4355f04b46f7324
Kevin Abaquita <nivekbusinesses@gmail.com>
2025-09-02 13:26:04 +0800

    update test 1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ latestVersion: "1.0.7", date: "04-28-25" });
+    res.json({ latestVersion: "1.0.8", date: "09-02-25" });
 });
 
 const PORT = process.env.PORT || 3000;
