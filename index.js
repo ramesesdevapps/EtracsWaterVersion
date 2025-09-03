ac2bdc0234f551d3b08bba99cba089fc94f3528c
Kevin Abaquita <nivekbusinesses@gmail.com>
2025-09-03 13:50:59 +0800

    alert replaced with custom modal

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ latestVersion: "1.0.8", date: "09-02-25" });
+    res.json({ latestVersion: "1.0.9", date: "09-03-25" });
 });
 
 const PORT = process.env.PORT || 3000;
