2e79ebb9ccb78c5475863a9baae41679742e171e
Cayenne Ezra C. Gagno <cayenneezra@gmail.com>
2025-10-21 08:36:18 +0800

    added bluetooth permission

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ latestVersion: "1.0.9", date: "09-03-25" });
+    res.json({ latestVersion: "1.0.10", date: "10-21-25" });
 });
 
 const PORT = process.env.PORT || 3000;
