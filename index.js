b9a13472d813807c1b56a686382df2a3f0fb45bd
ramesesdevapps <rameses.devapps@gmail.com>
2026-06-17 08:44:44 +0800

    update version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ latestVersion: "1.0.11", date: "06-02-26" });
+    res.json({ latestVersion: "1.0.12", date: "06-17-26" });
 });
 
 const PORT = process.env.PORT || 3000;
